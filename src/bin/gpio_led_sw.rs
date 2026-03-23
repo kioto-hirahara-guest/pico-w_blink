0e18df6403929661d0a408fd20d4c2b3025be51f
kioto-hirahara-guest <kioto_hirahara_gst@jp.honda>
2026-03-23 20:48:36 +0900

    Add GPIO LED with switch program

---
+++ src/bin/gpio_led_sw.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Pull, Output, Input};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let mut led = Output::new(p.PIN_14, Level::Low);
+    let sw = Input::new(p.PIN_16, Pull::Down);
+
+    loop {
+        if sw.is_high() {
+            led.set_high();
+        } else {
+            led.set_low();
+        }
+    }
+}
